ccb609fdd972f438c342fe6d2011bafd69673ffb
jonnyqian <qiankechong@126.com>
2017-03-14 11:00:46 +0800

    2345678
    123456

---
+++ MedicCalc/Classes/All(全部)/View/AegVC/AgeVC.h
@@ -0,0 +1,19 @@
+//
+//  AgeVC.h
+//  MedicCalc
+//
+//  Created by Jonnyqian on 2017/3/7.
+//  Copyright © 2017年 Jonnyqian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AgeVC : UIViewController
+{
+//    UILabel *_showLabel;
+
+}
+@property (nonatomic,strong) UILabel *showLabel;;
+
+@property (nonatomic,strong)UIDatePicker *datePicker;
+@end
---
+++ MedicCalc/Classes/More(更多)/Controller/BaseSettingTVC.h
@@ -0,0 +1,21 @@
+//
+//  BaseSettingTVC.h
+//  MedicCalc
+//
+//  Created by Jonnyqian on 2017/3/1.
+//  Copyright © 2017年 Jonnyqian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SettingItem.h"
+#import "SettingGroup.h"
+#import "SettingItemArrow.h"
+#import "SettingItemSwitch.h"
+#import "MoreSettingCell.h"
+
+@interface BaseSettingTVC : UITableViewController
+-(NSArray * )getGroups;
+@property(nonatomic,copy)NSString  *viewTitle;
+
+@end
